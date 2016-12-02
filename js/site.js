ded5c0ab368fde137398d8b1aef50a65fcff3d1b
Carver Harrison <pcarverb@hotmail.com>
2016-12-02 13:48:40 -0800

    Update site.js

--- js/site.js
+++ js/site.js
@@ -2,4 +2,6 @@ $("nav a").on("click", function(e) {
   $(".active").attr("class", "");
   $(this).attr("class", "active");
   $("title").text(`Dorper Static | ${$(this).text()}`);
+  $("section").hide();
+  $(`section[data-page=${$(this).text}]`).show();
 });
